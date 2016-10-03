6086b542ed49d69a8ae88c28ece647f7255d99e9
Chandini Peruri <chandini.peruri@gmail.com>
2016-10-03 14:04:49 -0700

    Add project files.

---
+++ Julia/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Julia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c0Val = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.c1Val = new System.Windows.Forms.Label();
+            this.Graph_Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c0Val
+            // 
+            this.c0Val.AutoSize = true;
+            this.c0Val.Location = new System.Drawing.Point(12, 101);
+            this.c0Val.Name = "c0Val";
+            this.c0Val.Size = new System.Drawing.Size(25, 13);
+            this.c0Val.TabIndex = 0;
+            this.c0Val.Text = "c0 :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // c1Val
+            // 
+            this.c1Val.AutoSize = true;
+            this.c1Val.Location = new System.Drawing.Point(12, 154);
+            this.c1Val.Name = "c1Val";
+            this.c1Val.Size = new System.Drawing.Size(25, 13);
+            this.c1Val.TabIndex = 3;
+            this.c1Val.Text = "c1 :";
+            // 
+            // Graph_Button
+            // 
+            this.Graph_Button.Location = new System.Drawing.Point(12, 201);
+            this.Graph_Button.Name = "Graph_Button";
+            this.Graph_Button.Size = new System.Drawing.Size(75, 23);
+            this.Graph_Button.TabIndex = 4;
+            this.Graph_Button.TabStop = false;
+            this.Graph_Button.Text = "Graph";
+            this.Graph_Button.UseVisualStyleBackColor = true;
+            this.Graph_Button.Click += new System.EventHandler(this.Graph_Button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(109, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_PaintEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 920);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Graph_Button);
+            this.Controls.Add(this.c1Val);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.c0Val);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label c0Val;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label c1Val;
+        private System.Windows.Forms.Button Graph_Button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Julia/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Julia
+{
+    public partial class Form1 : Form
+    {
+        //variable declarations
+        //test values -0.70176-0.3842i
+        Boolean readyToGraph= false;
+        double c0, c1;
+        double dx;
+        double x0, y0, newX, newY;
+
+        Color col;
+        Brush b;
+        public Form1()
+        {
+            
+            dx = 0.005;
+            InitializeComponent();
+        }
+
+        private void PictureBox_PaintEvent(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // loop through every pixel
+            if (readyToGraph)
+            {
+                
+                //loop through every pixel
+                for(float i=0; i<800; i++)
+                    for(float j =0; j<800; j++)
+                    {
+                        //calculate the initial real and imaginary parts based on pixel location, position values
+                        x0 = (i*dx)-2;
+                        y0 = -((j*dx)-2);
+                        int k;
+                        for(k =0; k<1024; k++) { 
+                        //new values
+                        newX = x0 * x0 - y0 * y0 +c0;
+                        newY = 2 * x0 * y0 +c1;
+                        //set values
+                        x0 = newX;
+                        y0 = newY;
+                            //if the point is outside the circle with radius 2, stop
+                            if ((newX * newX + newY * newY) > 16)
+                                break;
+                        }
+                            int c = k % 256;
+                            col = Color.FromArgb(0, c, 0);
+                            b = new SolidBrush(col);
+                            //draw the pixel
+                            g.FillRectangle(b, i, j, 1, 1);
+                    }
+            }
+        }
+        private void Graph_Button_Click(object sender, EventArgs e)
+        {
+            // Take c0 and c1 inputs
+            try
+            {
+                //read and save the c0 and c1 values from user input 
+                c0 = Convert.ToDouble(textBox1.Text);
+                c1 = Convert.ToDouble(textBox2.Text);
+                readyToGraph = true;
+                //calls the picturebox
+                pictureBox1.Refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Exeption occurs:" + ex);
+            }
+           
+        } 
+        
+    }
+}
